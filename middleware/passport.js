12cf53a5f625c9302baa15c000c3285237940177
lucifer <xifa@tsign.cn>
2021-04-27 20:47:23 +0800

    fix:  从 cookie 中解出来格式不对了

--- middleware/passport.js
+++ middleware/passport.js
@@ -1,5 +1,6 @@
 const fetch = require("node-fetch");
 const { encrypt, decrypt } = require("../utils/crypto");
+const { success, fail } = require("../utils/request");
 
 const secret = process.env.secret;
 
@@ -26,7 +27,7 @@ module.exports = async function checkAuth(ctx, next) {
           const duser = JSON.parse(duserStr);
 
           if (db.find((q) => q.login === duser.login)) {
-            ctx.body = duser;
+            ctx.body = success(duser);
             return;
           }
         } catch (err) {
