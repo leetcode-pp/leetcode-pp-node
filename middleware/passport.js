f0b3791e114b54f5f8bdfbf05d4387ca2b5fbc85
lucifer <xifa@tsign.cn>
2021-04-27 16:57:31 +0800

    fix: not return

--- middleware/passport.js
+++ middleware/passport.js
@@ -16,71 +16,71 @@ module.exports = async function checkAuth(ctx, next) {
   }
   if (ctx.session.user) {
     await next();
-    return;
-  }
-  const token = ctx.cookies.get("token");
+  } else {
+    const token = ctx.cookies.get("token");
 
-  if (token) {
-    const duserStr = decrypt(token);
-    if (duserStr) {
-      try {
-        const duser = JSON.parse(duserStr);
+    if (token) {
+      const duserStr = decrypt(token);
+      if (duserStr) {
+        try {
+          const duser = JSON.parse(duserStr);
 
-        if (db.find((q) => q.login === duser.login)) {
-          ctx.body = duser;
-          return;
+          if (db.find((q) => q.login === duser.login)) {
+            ctx.body = duser;
+            return;
+          }
+        } catch (err) {
+          console.log("token 解析失败:", err);
         }
-      } catch (err) {
-        console.log("token 解析失败:", err);
       }
     }
-  }
-  const code = ctx.query.code;
-  const { access_token } = await fetch(
-    `https://github.com/login/oauth/access_token?code=${code}&client_id=${clientId}&client_secret=${secret}`,
-    {
-      method: "POST",
+    const code = ctx.query.code;
+    const { access_token } = await fetch(
+      `https://github.com/login/oauth/access_token?code=${code}&client_id=${clientId}&client_secret=${secret}`,
+      {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+        },
+      }
+    ).then((res) => res.json());
+
+    const user = await fetch("https://api.github.com/user", {
       headers: {
         Accept: "application/json",
+        Authorization: `token ${access_token}`,
       },
+    }).then((res) => res.json());
+
+    if (db.find((q) => q.login === user.login)) {
+      ctx.cookies.set(
+        "token",
+        encrypt(
+          Buffer.from(
+            JSON.stringify({
+              ...user,
+              pay: true,
+            }),
+            "utf8"
+          )
+        ),
+        {
+          httpOnly: false,
+          expires: new Date(24 * 60 * 60 * 1000 + Date.now()),
+        }
+      );
+
+      ctx.session.user = {
+        ...user,
+        pay: true,
+      };
+    } else {
+      ctx.session.user = {
+        ...user,
+        pay: false,
+      };
     }
-  ).then((res) => res.json());
 
-  const user = await fetch("https://api.github.com/user", {
-    headers: {
-      Accept: "application/json",
-      Authorization: `token ${access_token}`,
-    },
-  }).then((res) => res.json());
-
-  if (db.find((q) => q.login === user.login)) {
-    ctx.cookies.set(
-      "token",
-      encrypt(
-        Buffer.from(
-          JSON.stringify({
-            ...user,
-            pay: true,
-          }),
-          "utf8"
-        )
-      ),
-      {
-        httpOnly: false,
-        expires: new Date(24 * 60 * 60 * 1000 + Date.now()),
-      }
-    );
-
-    ctx.session.user = {
-      ...user,
-      pay: true,
-    };
-  } else {
-    ctx.session.user = {
-      ...user,
-      pay: false,
-    };
+    await next();
   }
-
-  await next();
 };
