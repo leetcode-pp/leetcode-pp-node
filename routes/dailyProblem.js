e25c90cb2f981bd23623c69b943e5e4aff15680d
lucifer <xifa@tsign.cn>
2021-04-30 14:59:59 +0800

    chore: 仅支持历史查询，不支持未来时间

--- routes/dailyProblem.js
+++ routes/dailyProblem.js
@@ -12,6 +12,7 @@ router.get("/api/v1/daily-problem", async (ctx) => {
   // 1. 如果用户指定了时间，则获取指定时间，否则获取今天
   // 2. 根据上一步计算的时间和活动开始时间计算当前是 Day 几
   // 3. 根据 Day 几 计算出具体返回哪一个题目
+  // ！！注意： 如果用户指定的时间大于今天，则返回”题目不存在，仅支持查询历史每日一题“
 
   const date = ctx.query.date; // 用户指定的实际
 
@@ -71,6 +72,8 @@ router.get("/api/v1/daily-problem", async (ctx) => {
 
 router.get("/api/v1/daily-problem/solution", async (ctx) => {
   // 逻辑和上面类似，只是返回值为 Markdown
+
+  // ！！注意： 如果用户指定的 day 大于今天对应的day，则返回”题解不存在，仅支持查询历史官方题解“
 
   const day = ctx.query.day || getCurrentDay(); // 用户指定的实际第几天（注意这里的 day 是数字，含义为第 day 天）
   if (day in solutions) {
