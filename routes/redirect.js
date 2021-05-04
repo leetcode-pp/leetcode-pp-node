e68e7c215de978f86132d699179f5b7503546973
lucifer <xifa@tsign.cn>
2021-05-04 14:47:12 +0800

    fix: typo

--- routes/redirect.js
+++ routes/redirect.js
@@ -1,7 +1,7 @@
 const router = require("koa-router")();
 const fetch = require("node-fetch");
 router.get("*", async (ctx, next) => {
-  if (ctx.path.startsWith("api")) {
+  if (ctx.path.startsWith("/api")) {
     next();
   } else {
     ctx.body = await fetch("https://leetcode-solution.cn/91").then((res) =>
