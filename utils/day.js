9f206192a1ddb7d91f691ce4bd791e1682c8ac0d
lucifer <xifa@tsign.cn>
2021-05-08 18:54:19 +0800

    北京时间测试

--- utils/day.js
+++ utils/day.js
@@ -4,6 +4,7 @@ const MS_PER_DAY = 24 * 60 * 60 * 1000;
 
 module.exports = {
   getDay(date = new Date().getTime()) {
+    console.log(startTime);
     return ((date - startTime + MS_PER_DAY - 1) / MS_PER_DAY) >> 0;
   },
 };
