a0450b53395d46e01ff0593df2ce466968c92d4f
robot <xifa@tsign.cn>
2021-05-12 11:44:39 +0800

    feat: automate generate data

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,5 +1,6 @@
 if [[ `git status --porcelain` ]]; then
     git config --global user.name 'robot'
-    git commit -am "feat: automate generate data"
+    git add .
+    git commit -m "feat: automate generate data"
     git push
 fi
