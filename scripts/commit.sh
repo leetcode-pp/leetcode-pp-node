b3a00392af1d84fecc5b93c02c76448c03140f3b
robot <xifa@tsign.cn>
2021-05-13 17:40:35 +0800

    feat: commit if nessasry

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,6 +1,6 @@
 if [[ `git status --porcelain` ]]; then
     git config --global user.name 'robot'
     git add .
-    git commit -m "feat: automate generate data"
+    git commit -m "commit automatically"
     git push
 fi
