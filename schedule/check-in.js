77a9bd6098276177afa6ab37f9455f26e1e079f5
robot <xifa@tsign.cn>
2021-05-14 11:19:50 +0800

    feat: checkin

---
+++ schedule/check-in.js
@@ -0,0 +1,53 @@
+const { getDay } = require("../utils/day");
+const us = require("../static/users/index");
+const mySolutions = require("../static/my/solutions.json");
+
+// 下面代码没有考虑一个人中途加入，那么他没有加入之前也会被统计为未打卡。
+const allUsers = JSON.parse(JSON.stringify(us));
+
+function run(n) {
+  // 返回7天内所有已经打卡的
+  function checkWithinNDays(n) {
+    const users = {};
+    const day = getDay();
+    if (day <= n) return allUsers;
+    for (const name in mySolutions) {
+      const solutions = mySolutions[name];
+      if (!(name in allUsers)) {
+        console.log(`震惊! ${name} 竟然发生了这种事情，真相令人唏嘘！`);
+        continue;
+      }
+      let i = Math.max(day - n, getDay(allUsers[name].createTime));
+
+      let count = 0;
+
+      while (i < day) {
+        if (!solutions[i]) {
+          count += 1;
+        } else {
+          count = 0;
+        }
+        if (count == n) {
+          break;
+        }
+        i++;
+      }
+
+      if (count < n) {
+        users[name] = true;
+      }
+    }
+    return users;
+  }
+
+  function diff(A, B) {
+    for (const name in B) {
+      delete A[name];
+    }
+    return Object.keys(A);
+  }
+
+  console.log(diff(allUsers, checkWithinNDays(n)));
+}
+
+run(4);
