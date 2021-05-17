5fd523f5c23d7aeab5cbc93ec1ca126212c01f47
robot <xifa@tsign.cn>
2021-05-17 11:05:27 +0800

    commit automatically

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -1,5 +1,6 @@
 if [[ `git status --porcelain` ]]; then
     git config --global user.name 'robot'
+    git config --global core.mergeoptions --no-edit
     git add .
     git commit -m "commit automatically"
     if ! git push ; then
