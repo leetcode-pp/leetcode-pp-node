1b99edd114daea12f0f2b4c4e36f0099d4409bd5
robot <xifa@tsign.cn>
2021-05-17 11:08:18 +0800

    commit automatically

--- scripts/commit.sh
+++ scripts/commit.sh
@@ -3,6 +3,7 @@ if [[ `git status --porcelain` ]]; then
     git config --global core.mergeoptions --no-edit
     git add .
     git commit -m "commit automatically"
+    # if someone else commit at the same time, you must pull before pushing and 'GET an error'
     if ! git push ; then
         git pull
         git push
