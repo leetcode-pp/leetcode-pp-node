df9f41b0563c84cf78034b0efec0eb12807d6cff
robot <xifa@tsign.cn>
2021-05-18 11:57:56 +0800

    test

--- middleware/mockUserInfo.js
+++ middleware/mockUserInfo.js
@@ -4,8 +4,8 @@ module.exports = ({ whitelist = [] }) =>
       ctx.session = {};
     }
     ctx.session.user = {
-      // login: "lilyzhaoyilu",
-      login: "YanFaFa",
+      login: "lilyzhaoyilu",
+      // login: "YanFaFa",
       // login: "shuo0314",
       // login: "azl397985856",
       avatar_url: "https://avatars.githubusercontent.com/u/12479470?v=4",
