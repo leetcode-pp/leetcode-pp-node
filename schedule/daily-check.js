d4ddb0ed12e985322b9511503de299ea05280b0c
robot <xifa@tsign.cn>
2021-05-24 10:52:07 +0800

    feat: 每天全量更新一次

--- schedule/daily-check.js
+++ schedule/daily-check.js
@@ -6,6 +6,7 @@ const { owner, repo } = require("../config/index");
 
 const solutions = require("../static/solution/solutions.json");
 const mySolutions = require("../static/my/solutions.json");
+const { get } = require("http");
 
 const octokit = new Octokit({ auth: process.env.issueToken });
 function getAllPages(i, issue_number) {
@@ -46,6 +47,9 @@ async function run(d) {
         onTime: getDay(new Date(comment.created_at).getTime()) <= d,
       };
     });
+    mySolutions["_meta_"] = {
+      lastUpdateTime = new Date().getTime()
+    }
     fs.writeFileSync(
       path.resolve(__dirname, "../static/my/solutions.json"),
       JSON.stringify(mySolutions)
@@ -55,9 +59,17 @@ async function run(d) {
 const MS_PER_HOUR = 1 * 60 * 60 * 1000;
 const TODAY = getDay(new Date().getTime() - MS_PER_HOUR); // 获取今天‘的题目。 为了照顾一些人， 我们凌晨一点统计昨天的，而不是当天的。
 
+// 仅更新当天的
+if (getDay(TODAT) - getDay(mySolutions["__meta__"].lastUpdateTime) < 1) {
+  run(TODAY)
+} else {
+// 更新历史所有的，每天仅全量更新一次
 // 1. 记录打卡数据
 // 2. 修正之前的数据错误
 // 3. 登记补卡信息
 for (let d = 1; d <= TODAY; d++) {
   run(d);
 }
+} 
+
+
