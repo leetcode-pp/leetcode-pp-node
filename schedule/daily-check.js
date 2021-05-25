0efd3b22e7c8912bd2fa57f4ec7f47a31923680f
robot <xifa@tsign.cn>
2021-05-25 20:39:21 +0800

    fix: onTime

--- schedule/daily-check.js
+++ schedule/daily-check.js
@@ -9,6 +9,10 @@ const mySolutions = require("../static/my/solutions.json");
 const meta = require("../static/meta.json");
 
 const octokit = new Octokit({ auth: process.env.issueToken });
+
+const MS_PER_HOUR = 1 * 60 * 60 * 1000;
+const TODAY = getDay(new Date().getTime() - MS_PER_HOUR); // 获取今天‘的题目。 为了照顾一些人， 我们凌晨一点统计昨天的，而不是当天的。
+
 function getAllPages(i, issue_number) {
   return octokit.rest.issues
     .listComments({
@@ -39,12 +43,15 @@ async function run(d) {
       }
       // 由于下面的一行代码，导致了会插入一个完全空的行。这就是因为打过卡，但是都不是当天打的
       if (comment.body.length < 20) return;
+      if (mySolutions[login][d - 1].onTime !== void 0) return; // 如果打卡过或者补卡过就不同步了
       // 由于索引从 1 开始，因此需要再减去 1。
       mySolutions[login][d - 1] = {
         // title: problem.title,
         url: comment.html_url,
         body: comment.body,
-        onTime: getDay(new Date(comment.created_at).getTime()) <= d,
+        // 只有当天的才标记为 onTime。防止创建一个空白的 comment，之后再编辑
+        onTime:
+          TODAY === d && getDay(new Date(comment.created_at).getTime()) <= d, // 有可能早出题，防止早打卡的人没被统计进去，所以用 <= 。
       };
     });
     meta.dailyCheck.lastUpdateTime = new Date().getTime();
@@ -60,9 +67,6 @@ async function run(d) {
     );
   }
 }
-
-const MS_PER_HOUR = 1 * 60 * 60 * 1000;
-const TODAY = getDay(new Date().getTime() - MS_PER_HOUR); // 获取今天‘的题目。 为了照顾一些人， 我们凌晨一点统计昨天的，而不是当天的。
 
 // 仅更新当天的
 if (getDay(TODAY) - getDay(meta.dailyCheck.lastUpdateTime) < 1) {
