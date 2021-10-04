62ee8d903bd82884d352cfeddfecdd1546f1c862
robot <xifa@tsign.cn>
2021-10-04 21:43:27 +0800

    feat: reCard

---
+++ scripts/reCard.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+const name = 'yyangeee';
+
+const us = require("../static/users/index");
+
+us[name].noCheck = false
+
+
+
+us[name].createTime = new Date().getTime()
+
+
+fs.writeFileSync(
+    path.resolve(__dirname, "../static/users/index.json"),
+    JSON.stringify(us)
+  );
