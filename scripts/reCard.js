e58d79496268cd2bc85700a7461ffd45a7e403f0
robot <xifa@tsign.cn>
2021-11-04 12:42:48 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "xulli1996".trim().toLocaleLowerCase();
+const name = "zhuzuojun".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
