d1d2beaf6a7a68b9d6c31bf0e66561cf93469664
robot <xifa@tsign.cn>
2021-11-10 19:55:43 +0800

    名单

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "qixuan-code".trim().toLocaleLowerCase();
+const name = "lemon-hoyoung".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
