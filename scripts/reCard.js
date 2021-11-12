825f52c6f6a3b017cc0d0f69affec9562cd68484
robot <xifa@tsign.cn>
2021-11-12 13:19:41 +0800

    bu补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "lemon-hoyoung".trim().toLocaleLowerCase();
+const name = "zhuzuojun".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
