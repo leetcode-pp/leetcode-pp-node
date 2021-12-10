c4cd6b37366f10c069ce23390c9a8e3ef4446199
robot <xifa@tsign.cn>
2021-12-10 20:04:54 +0800

    第六期

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ console.log(db)
 module.exports = {
   leetcodeConfig,
   owner: "leetcode-pp",
-  repo: "91alg-5-daily-check",
+  repo: "91alg-6-daily-check",
   startTime: startTime.getTime(),
   secret: process.env.secret,
   clientId: "c16b80e7b58a5a007157",
