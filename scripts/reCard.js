12e7483a46444a1026f61ad6beecda1ae31a7d20
robot <xifa@tsign.cn>
2022-02-07 17:30:59 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "ivangin".trim().toLocaleLowerCase();
+const name = "bluetomlee".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
