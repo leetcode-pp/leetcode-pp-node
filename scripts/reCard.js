877f5c258b544403123f4ebd52744356d533b572
robot <xifa@tsign.cn>
2022-02-10 21:19:37 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,17 +1,19 @@
 const fs = require("fs");
 const path = require("path");
-const name = "luhnn120".trim().toLocaleLowerCase();
+const name = "MissNanLan".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
 if (!us[name]) {
   console.log("用户名错误");
+} else {
+  us[name].noCheck = false;
+
+  us[name].createTime = new Date().getTime();
+
+  fs.writeFileSync(
+    path.resolve(__dirname, "../static/users/index.json"),
+    JSON.stringify(us)
+  );
 }
-us[name].noCheck = false;
 
-us[name].createTime = new Date().getTime();
-
-fs.writeFileSync(
-  path.resolve(__dirname, "../static/users/index.json"),
-  JSON.stringify(us)
-);
