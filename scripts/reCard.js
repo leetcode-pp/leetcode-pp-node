b50d2965f8292c6048e896bef441864ed5b25f99
robot <xifa@tsign.cn>
2022-02-20 11:19:44 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "rockydale".trim().toLocaleLowerCase();
+const name = "Tao-Mao".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
