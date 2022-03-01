396af9fe8ab2f211b6c3db7a77aec07523620180
robot <xifa@tsign.cn>
2022-03-01 15:56:55 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "Size-of".trim().toLocaleLowerCase();
+const name = "accentualtor".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
