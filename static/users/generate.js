a654c98bd0d6305813fd9baa551d7e21245d49d3
robot <xifa@tsign.cn>
2022-04-22 22:50:07 +0800

    fix: incremental time

--- static/users/generate.js
+++ static/users/generate.js
@@ -7,59 +7,60 @@ const users = require("./index.json");
 const meta = require("../meta.json");
 
 async function run(incremental = true) {
-  const octokit = new Octokit({ auth: process.env.issueToken });
-  const ps = [];
-  for (const name in db) {
-    if (users[name] && users[name].createTime && incremental) continue;
+	const octokit = new Octokit({ auth: process.env.issueToken });
+	const ps = [];
+	for (const name in db) {
+		if (users[name] && users[name].createTime && incremental) {
+			continue;
+		}
 
-    ps.push(
-      octokit.rest.users
-        .getByUsername({ username: name })
-        .then((res) => res.data)
-        .then((user) => {
-          if (!(name in users) || !users[name].createTime) {
-            users[name] = {
-              ...user,
-              createTime: new Date().getTime(),
-            };
-          } else {
-            //  createTime 字段不能更新，其他都可以更新
-            const c = users[name].createTime;
-            users[name] = {
-              ...users[name],
-              ...user,
-              createTime: c,
-            };
-          }
-        })
-        .catch((err) =>
-          console.log(
-            `name ${name} is invalid. detail: ${err && err.message ? err.message : err
-            }`
-          )
-        )
-    );
-  }
+		ps.push(
+			octokit.rest.users
+				.getByUsername({ username: name })
+				.then((res) => res.data)
+				.then((user) => {
+					if (!(name in users) || !users[name].createTime) {
+						users[name] = { ...user, createTime: new Date().getTime() };
+					} else {
+						//  createTime 字段不能更新，其他都可以更新
+						const c = users[name].createTime;
+						users[name] = { ...users[name], ...user, createTime: c };
+					}
+				})
+				.catch(
+					(err) =>
+						console.log(
+							`name ${name} is invalid. detail: ${
+								err && err.message ? err.message : err
+							}`,
+						),
+				),
+		);
+	}
 
-  return Promise.all(ps);
+	return Promise.all(ps);
 }
 let incremental = true;
 
 // 七天全量更新一次
 const lastUpdateTime = meta.users ? meta.users.lastUpdateTime : -1;
 
-if (new Date().getTime() - lastUpdateTime >= 7 * 24 * 60 * 60 * 1000) {
-  incremental = false;
+if ((new Date().getTime() - lastUpdateTime) >= (7 * 24 * 60 * 60 * 1000)) {
+	incremental = false;
 }
 
-run(incremental).then(() => {
-  fs.writeFileSync(__dirname + "/index.json", JSON.stringify(users));
-  if (!meta.users) {
-    meta.users = {};
-  }
-  meta.users.lastUpdateTime = new Date().getTime();
-  fs.writeFileSync(
-    path.resolve(__dirname, "../meta.json"),
-    JSON.stringify(meta)
-  );
-});
+run(incremental)
+	.then(() => {
+		fs.writeFileSync(__dirname + "/index.json", JSON.stringify(users));
+		if (!meta.users) {
+			meta.users = {};
+		}
+		if (incremental) {
+			// 上次全量更新时间
+			meta.users.lastUpdateTime = new Date().getTime();
+		}
+		fs.writeFileSync(
+			path.resolve(__dirname, "../meta.json"),
+			JSON.stringify(meta),
+		);
+	});
