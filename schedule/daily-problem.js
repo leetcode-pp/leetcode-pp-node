2b0cae03e3719b45032ffdcbc2d8c520bcbdaa25
robot <xifa@tsign.cn>
2022-04-23 11:58:05 +0800

    go

--- schedule/daily-problem.js
+++ schedule/daily-problem.js
@@ -15,73 +15,83 @@ const solution = solutions[currentDay];
 
 // generate content for issues
 const generateIssueContent = ({ title, link, pres, description, whys }) => {
-  return `# ${title}
+	return `# ${title}
   ## 入选理由    
-  ${(whys || []).reduce((acc, cur, index) => {
-    return `${acc}    
+  ${
+		(whys || []).reduce(
+			(acc, cur, index) => {
+				return `${acc}    
   ${index + 1}. ${cur}`;
-  }, "") || "暂无"
-    }
+			},
+			"",
+		) || "暂无"
+	}
   ## 题目地址    
   [${link}](${link})
   ## 前置知识    
-  ${(pres || []).reduce((acc, cur) => {
-      return `${acc}    
+  ${
+		(pres || []).reduce(
+			(acc, cur) => {
+				return `${acc}    
   - ${cur}`;
-    }, "") || "暂无"
-    }
+			},
+			"",
+		) || "暂无"
+	}
   ## 题目描述    
   ${description}
     `;
 };
 
 const generateIssueTitle = ({ day, title }) =>
-  `【Day ${day} 】${chineseDate(
-    new Date().getTime() + MS_PER_HOUR
-  ).toLocaleDateString("en-CA")} - ${title}`;
+	`【Day ${day} 】${chineseDate(new Date().getTime() + MS_PER_HOUR).toLocaleDateString(
+		"en-CA",
+	)} - ${title}`;
 
 async function run(solution) {
-  await octokit.rest.issues.listForRepo({
-    labels: String(solution.day),
-    owner,
-    repo
-  }).then(async (res) => {
-    const { data } = res;
-    if (data.length > 0) {
-      throw new Error(`Day ${solution.day} has been published.`);
-    }
-  });
-  const { data } = await octokit.rest.issues.create({
-    owner,
-    repo,
-    title: generateIssueTitle(solution),
-    body: generateIssueContent(solution),
-    labels: (solution.tags || []).concat(String(solution.day)),
-  });
+	// 假设不注释下面代码，如果 create issue 成功的时候挂了导致没有 commit 成功，以后就永远无法更新 issue_number 了
+	// 因此暂时解决方法是前端不根据 issue_number 判断是否创建了 issue，而是无脑相信创建了。
+	await octokit.rest.issues
+		.listForRepo({ labels: String(solution.day), owner, repo })
+		.then(async (res) => {
+			const { data } = res;
+			if (data.length > 0) {
+				throw new Error(`Day ${solution.day} has been published.`);
+			}
+		});
+	const { data } = await octokit.rest.issues.create({
+		owner,
+		repo,
+		title: generateIssueTitle(solution),
+		body: generateIssueContent(solution),
+		labels: (solution.tags || []).concat(String(solution.day)),
+	});
 
-  solution.issue_number = data.number;
+	solution.issue_number = data.number;
 
-  fs.writeFileSync(
-    path.resolve(__dirname, "../static/solution/solutions.json"),
-    JSON.stringify({
-      ...solutions,
-      [currentDay]: solution,
-    })
-  );
+	fs.writeFileSync(
+		path.resolve(__dirname, "../static/solution/solutions.json"),
+		JSON.stringify({ ...solutions, [currentDay]: solution }),
+	);
 }
 
 // 当前有题解，并且今天的题目还没发布，就自动创建一个 issue
 if (solution && !solution.issue_number) {
-  run(solution);
+	run(solution);
 }
 
 // 预打卡
 if (meta && !meta.preCard) {
-  meta.preCard = true;
-  run({ title: '预打卡', link: '此处正常应该是链接', description: '让大家熟悉如何打卡', day: 0, tags: ['预打卡'] })
-  fs.writeFileSync(
-    path.resolve(__dirname, "../static/meta.json"),
-    JSON.stringify(meta)
-  );
+	meta.preCard = true;
+	run({
+		title: "预打卡",
+		link: "此处正常应该是链接",
+		description: "让大家熟悉如何打卡",
+		day: 0,
+		tags: ["预打卡"],
+	});
+	fs.writeFileSync(
+		path.resolve(__dirname, "../static/meta.json"),
+		JSON.stringify(meta),
+	);
 }
-
