2fcea6d23e00dca82927b6906e13d0c2e3de4397
robot <xifa@tsign.cn>
2022-04-24 23:12:19 +0800

    go

--- schedule/check-in.js
+++ schedule/check-in.js
@@ -101,7 +101,7 @@ function run(n) {
 		}
 		return Object.keys(A);
 	}
-	const graplist = Object.keys(checkWithinNDays(n));
+	const graplist = checkWithinNDays(n);
 	const blacklist = diff(allUsers, graplist).filter(
 		(name) => !/[A-Z]/.test(name),
 	);
@@ -109,7 +109,7 @@ function run(n) {
 	console.log(`no check within ${n} days`, JSON.stringify(blacklist));
 	console.log(`full check`, redlist);
 
-	for (const login of graplist) {
+	for (const login of Object.keys(graplist)) {
 		// 更新 users 信息。 如果七天没有打过卡，则直接不会更新到 users
 		if (!us[login]) {
 			us[login] = {};
