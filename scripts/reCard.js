94256675ad7a93d3a912095148e140568cc077fa
robot <xifa@tsign.cn>
2022-06-02 12:41:48 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "DivingCub".trim().toLocaleLowerCase();
+const name = "liuguang520-lab".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 
