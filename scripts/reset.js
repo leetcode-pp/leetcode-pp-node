4b653807e50b0c99f21ca6680d633e13757f0505
robot <xifa@tsign.cn>
2022-07-13 00:53:36 +0800

    go

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,36 +1,42 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const mySolutions = require("../static/my/solutions.json");
 const solutions = require("../static/solution/solutions.json");
+// 备份
+const season = 8;
 
 // 新的一期开始，重启！
 
 function start() {
+	for (const name in solutions) {
+		const my = [];
+		mySolutions[name] = my;
+		for (solution of mySolutions[name]) {
+			if (
+				solution && solution.url.startsWith(
+					`https://github.com/leetcode-pp/91alg-${season - 1}-daily-check`,
+				)
+			) {
+				my.push(solution);
+			} else {
+				my.push(null);
+			}
+		}
+	}
 
-    for (const name in solutions) {
-        const my = []
-        mySolutions[name] = my
-        for (solution of mySolutions[name]) {
-            if (solution && solution.url.startsWith("https://github.com/leetcode-pp/91alg-7-daily-check")) {
-                my.push(solution)
-            } else {
-                my.push(null)
-            }
-        }
-    }
+	fs.writeFileSync(
+		path.resolve(__dirname, "../static/my/solutions.json"),
+		JSON.stringify(mySolutions),
+	);
 
-    fs.writeFileSync(
-        path.resolve(__dirname, "../static/my/solutions.json"),
-        JSON.stringify(mySolutions)
-    );
+	for (const d in solutions) {
+		solutions[d].issue_number = void 0;
+	}
 
-    for (const d in solutions) {
-        solutions[d].issue_number = void 0
-    }
-
-    fs.writeFileSync(
-        path.resolve(__dirname, "../static/solution/solutions.json"),
-        JSON.stringify(solutions)
-    );
+	fs.writeFileSync(
+		path.resolve(__dirname, "../static/solution/solutions.json"),
+		JSON.stringify(solutions),
+	);
 }
-module.exports = { start }
+start();
+module.exports = { start };
