e3fd84292eba53e9d0829dc1fcf3fd50f5138284
robot <xifa@tsign.cn>
2022-07-13 01:04:55 +0800

    go

--- scripts/backupStatic.js
+++ scripts/backupStatic.js
@@ -4,9 +4,11 @@ const old = [
 	"static/lectures/lectures-by-id-4.json",
 	"static/lectures/lectures-by-id-5.json",
 	"static/lectures/lectures-by-id-6.json",
+	"static/lectures/lectures-by-id-7.json",
 	"static/lectures/lectures-by-category-4.json",
 	"static/lectures/lectures-by-category-5.json",
 	"static/lectures/lectures-by-category-6.json",
+	"static/lectures/lectures-by-category-7.json",
 	"static/solution/solutions-4.json",
 	"static/solution/solution-5.json",
 	"static/solution/solution-6.json",
@@ -17,6 +19,14 @@ const old = [
 	"static/users/index-5.json",
 	"static/users/index-6.json",
 ];
+
+for (let i = 7; i < 8; i++) {
+	old.push(`static/lectures/lectures-by-id-${i}.json`);
+	old.push(`"static/lectures/lectures-by-category-${i}.json`);
+	old.push(`static/solution/solution-${i}.json`);
+	old.push(`static/my/solutions-${i}.json`);
+	old.push(`static/users/index-${i}.json`);
+}
 
 for (const v of old) {
 	try {
