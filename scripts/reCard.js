71c0ebe5092906b19ac23f162548905674b09aa0
robot <xifa@tsign.cn>
2022-11-23 22:39:54 +0800

    名单

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "xqmmy".trim().toLocaleLowerCase();
+const name = "hyxupup".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
