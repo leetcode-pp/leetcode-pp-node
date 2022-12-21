31dfba5b4e8ab364dbad103899f62c84a30930bf
robot <xifa@tsign.cn>
2022-12-21 12:57:47 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "steven72574".trim().toLocaleLowerCase();
+const name = "astrking".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
