54a3c2ad595e7d164f908e9908cb4f2a16928552
robot <xifa@tsign.cn>
2023-02-08 19:29:56 +0800

    名单

--- config/users-10.js
+++ config/users-10.js
@@ -155,6 +155,14 @@ cf63255018	nuozhouzhang
 wzh18804001869	wangzh0114
 roc940922	RocJeMaintiendrai
 cf63255018	nuozhouzhang
+xingchenlhx	hughlin07
+hundanxingxiaobao	Christina-Soda
+15275150257	zhangyong-hub
+miatasoyes	sye9286
+cst98678	csthaha
+xb798298436	xb798298436
+WitsanChen	WitsanChen
+WitsanChen	WitsanChen
 `;
 
 const unknowns = [
@@ -191,6 +199,7 @@ const unknowns = [
   "wang18804001869",
   "16602112714",
   "lmx55011",
+  "15275150257"
 ];
 const users = {};
 const lines = data.split(/\n/);
