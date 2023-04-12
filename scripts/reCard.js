f2a313c0419389eccbff2123b3d9a78a6d4ed9e6
robot <xifa@tsign.cn>
2023-04-12 15:44:38 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "Size-of".trim().toLocaleLowerCase();
+const name = "huizsh".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
