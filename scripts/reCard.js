e809c81a3fa1ab7a9833a8ac87829cbbcc04ae47
robot <xifa@tsign.cn>
2023-04-21 17:10:14 +0800

    go

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "jiujingxukong".trim().toLocaleLowerCase();
+const name = "linlizzz".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
