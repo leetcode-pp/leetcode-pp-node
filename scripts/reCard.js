f62308a389c9c57555b08ee19613330a1b3f0e17
robot <xifa@tsign.cn>
2023-05-08 23:48:20 +0800

    将已更新

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "SoSo1105".trim().toLocaleLowerCase();
+const name = "aswrise".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
