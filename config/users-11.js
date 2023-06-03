94db81ea0442e326e8668e2464469e757ced6a2c
robot <xifa@tsign.cn>
2023-06-03 18:17:44 +0800

    第 11 期

---
+++ config/users-11.js
@@ -0,0 +1,276 @@
+const data = `
+scncgoo	seanutf
+laoju0	AngryChocobo
+jliu0185	el1as7
+xjy_2584	nikojxie
+Hustry	munmust
+qx150604(千山慕雪)	Panfen
+spryti	adaex
+cushu_96	coder-cushu
+kanmiian	kanmiian
+airwalkers	airwalkers
+fylz_flyz	ZhuMengCheng
+hanchutianji622	richypang
+LoveYouWenQinAllLife	hshen_11@163.com
+ginkela	Ginkela
+Fear_of_Jay	WANGjay408
+w87100992	Serrust
+丁	xiaoDingc
+Sayuri_Watanabe	Dominique9898
+zenmehuiyourenyong	zpbc007
+l90032190	wangqianqian202301
+李晶	lijing1024
+Sunlitskylines-onlyu	Ryanbaiyansong
+yj920952933	yan-jie
+joemonkeylee	joemonkeylee
+13852991066	LIMBO42
+wrc165677	Wl678
+wrc165678	wl678
+xiaopawnye	lp1506947671
+Zephyr1943	zepherust
+小太阳	RichardTaoK
+Quieter_yuxi29	Quieter2018
+youyukao	FireHaoSky
+wxid_z2pbolnax5e112	xy147
+Dragon_FCL	DragonFCL
+zhangjh0501	zhangjiahuan17
+LoveYouWenQinAllLife	hshen11
+LoveYouWenQinAllLife	hshen11
+mengsixing_	mengsixing
+halcyon_lums	HalcyonZata
+kokobop22	sarah11qing
+chao00905	chao-cc
+wchat_four	Fuku-L
+wchat_four	Fuku-L
+wx_nawtain	snmyj@outlook.com
+gsy178069	import-ant
+YJACIVY	OASans
+BaoChengYo	c1f2h3
+fqk1129	feefeefee
+fqk1129	feefeefee
+zenwangzy	zenwangzy
+xinanande	Lrwhc
+dorapocket0402	dorapocket
+w_TRTW	GISW
+yldznf	chocolate-emperor
+13060206910	ezwkg@qq.com
+Adir42	texamc2
+Arrthure	KangQinSen
+huizsh	huizsh
+wx1398633077	luoanyang
+LIMBO_42	LIMBO42
+w1322021456	duke-github
+wangleilei1124	JETWLL
+13060206910	DrinkMorekaik
+_wuxiao_	wuxiaoShawn
+絮由	luckyoneday
+acelinguo	guoling0019
+web_code-5	lubaoqing
+wxid_o9o4m8nos7xe22	tzuikuo
+llforever126	chanceyliu
+chengh-nju	ha0cheng
+D-609860117	zhenxied
+ThatDoctor	JasonQiu
+Horace_uu	Horace7
+guid_shin	shin-guid
+zouzeyu1	CruiseYuGH
+ccslience	ccslience
+ccslience	ccslience
+Van2643097225	User-Vannnn
+du_dmq	Dianlandu
+wxid_8764367643811	for123s
+joriscai	joriscai
+X1AO___X1A	x1aox1a
+lt1114061614	zcytm3000
+lt1114061614	zcytm3000
+cc1046034010	jiujingxukong
+north-sea-coder	NorthSeacoder
+13250504940	954545647
+JingYuZhou-GoodLuck	JingYuZhou123
+18742055687	JadeLiu13
+drexolo	kevinding0218
+KrabbeJing	KrabbeJing
+KrabbeJing	KrabbeJing
+dujt2022	dujt-X
+dujt2022	dujt-X
+dujt2022	dujt-X
+zcdzyf	yufanzh
+_curlyJJ	erquaner 
+_curlyJJ	erquaner 
+wx_nawtain	snmyj
+KrabbeJing	krabbejing
+zmt1981208770	yuzmt
+zq945293035	franciszq
+liujiaqi3686	Jackielj
+ACutieCutiePie	SoSo1105
+Clarence5wo	Clarence5
+bzhang5121	bingzxy
+bo996233	HQHC
+ghs1205	HanshengGUO
+16602112714	xy147
+zsyliuxia	fw2043
+Helmet💦	954545647
+B13040513	AstrKing
+wxid_o9o4m8nos7xe22	tzuikuo
+pjd18260854526	pangjiadai
+kaoru_lino	kanmiian
+wxid_rdu0te34117m22	syh-coder
+xmsn90	wty9sky
+xmsn90	wty9sky
+aneureka2	aneureka
+Van2643097225	User-Vannnn
+wxid_8764367643811	for123s
+BaoAChengYo	c1f2h3
+qq1224832526	dinjufen
+nuo_mituan	linlizzz
+siezof8	Size-of
+sizeof8	Size-of
+Prosperidad_	Zoeyzyzyzy
+DiL_0349	RestlessBreeze
+bookyue	bookyue
+WYZMJD116	liuxiner
+CYCL1224	cyk1337
+shiyishuoshuo	shiyishuoshuo
+wang18804001869	wangzh0114
+wang18804001869	wangzh0114
+DrkMore_055	DrinkMorekaik
+DrkMore_055	DrinkMorekaik
+gnorux_42	Abby-xu
+16602112714	xy147
+lmx55011	alexno1no2
+LJJjiayouya	Jinjin680
+LJJjiayouya	Jinjin680
+zetalambda1	zol013
+wang18804001869	wangzh0114
+abby248	huifeng248
+cjbcbllm	zhenya-zhu
+wang18804001869	wangzh0114
+watermenlon827	jameswangxin
+LJJjiayouya	Jinjin680
+klay13_	jerry9926
+gaoyuan19880111	jackgaoyuan
+YUZI-1216	hjy-u
+lianghaoqin	leonalhq
+cf63255018	nuozhouzhang
+wzh18804001869	wangzh0114
+roc940922	RocJeMaintiendrai
+cf63255018	nuozhouzhang
+xingchenlhx	hughlin07
+hundanxingxiaobao	Christina-Soda
+15275150257	zhangyong-hub
+miatasoyes	sye9286
+cst98678	csthaha
+xb798298436	xb798298436
+WitsanChen	WitsanChen
+WitsanChen	WitsanChen
+Woyaogaimingziya	zhangyong-hub
+coffee_tans	1051450239@qq.com
+FungHiuwang	HWFrankFung
+LJ1433887494	liuajingliu
+CarolinaFu1998	yfu6
+Stellaaa_ma	jmastella
+paradisemusic	kofzhang
+LJY91505	JiangyanLiNEU
+sd8663	Meisgithub
+coffee_tans	HKTangShuo
+zzzzz_zzx	Elsa-zhang
+fuzzyfish	zzhilin
+qzflan	839781960
+K2046_2546	kangliqi1
+cuuze321	cuizezhou
+hatori1110	hatoriMain
+ywang525	ywang525
+Lydia_starry	lydia61
+wangaoxiang999	aoxiangw
+as13350539567	aswrise
+wuziyang0126	manwzy
+pkuphy	pkuphy
+A-qi___	xiaoq777
+5436888893	enrilwang
+Tachikomaa	FlipN9
+Ztrey__	Jetery
+withoutoctober	skyoct
+lwting_1999	elon-lau
+CYCL1224	cyk1337
+jiangjiayib	jyjsunny
+En176666	enrilwang
+wxid_tvkpmouwp0n522	uratora
+jdj	uratora
+ljqiswm	ljqchlsw
+injurehu	yingchehu
+kyong961115	kyo-tom
+w549261270	Bochengwan
+ys954244923	GuitarYs
+w549261270	Bochengwan
+820064276	frankzhang1990
+hoshuntin	sosdogecoin
+Cctrui17	C2tr
+hsj040625	levijain
+gridflow	wwewwt
+zhyuzh2002	Hermione666
+feiyang---X	FrannieX
+15895987576	zhangyu1131
+w2441505169	Diana21170648
+LY27597873	61hhh
+wbs15071133212	weiboshi
+CherylWulala	CherylWulala
+tianyu19920816	tianyu19920816
+18253710697	linfeng-sys
+`;
+
+const unknowns = [
+  "zoro-lulu",
+  "Dawn",
+  "宿愿Cc",
+  "Simple_TYJ",
+  "xiaohanlliu.0730",
+  "15396053927",
+  // "guid_shin", 第 X 期加入
+  "wxid_e52ejucgg5zt11",
+  "453593547",
+  "13719295799",
+  // "丁",
+  "cushu_96",
+  "kanmiian",
+  "wrc165677",
+  "13852991066",
+  // "Zephr1943",
+  // "wxid_z2pbolnax5e112",
+  "BaoChengYo",
+  "13060206910",
+  // "wxid_o9o4m8nos7xe22",
+  "web_code-5",
+  // "chengh-nju",
+  // "ThatDoctor",
+  "Van2643097225",
+  // "603289228",
+  "13250504940",
+  // "18742055687",
+  "16602112714",
+  // "wxid_rdu0te34117m22",
+  // "aneureka2",
+  "wang18804001869",
+  "16602112714",
+  // "lmx55011",
+  "15275150257",
+  "5436888893",
+  "wxid_tvkpmouwp0n522",
+  // "820064276"
+];
+const users = {};
+const lines = data.split(/\n/);
+for (const line of lines) {
+  const [wechat, github] = line.split(/\t/);
+  if (!wechat || !github) {
+    continue;
+  }
+  if (unknowns.includes(wechat)) {
+    continue;
+  }
+  users[wechat] = github;
+}
+
+exports.users = Object.values(users)
+  .filter(Boolean)
+  .map((v) => String.prototype.trim.call(v))
+  .map((v) => String.prototype.toLocaleLowerCase.call(v));
