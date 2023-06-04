ac54386354cf40d2268493a99f9770ad0a5b31cf
robot <xifa@tsign.cn>
2023-06-04 17:51:07 +0800

    名单

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
 const startTime = new Date("2023-06-10T00:00:00.000+08:00");
 // const { users } = require("./users-5");
-const { users } = require("./users-10");
+const { users } = require("./users-11");
 const us = require("../static/users/index");
 
 const whitelist = [
