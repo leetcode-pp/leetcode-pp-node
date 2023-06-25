770912a21d55845fa347f598a621c1fb34f69e3b
robot <xifa@tsign.cn>
2023-06-25 10:57:43 +0800

    go

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "jackgaoyuan".trim().toLocaleLowerCase();
+const name = "Master-guang".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
