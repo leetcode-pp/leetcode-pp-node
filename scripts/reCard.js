ba1f7e94b578fd575366e39f5d1a258ced69f7d8
robot <xifa@tsign.cn>
2023-12-20 13:59:15 +0800

    名单

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "andyli4".trim().toLocaleLowerCase();
+const name = "joe-the-plumber".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
