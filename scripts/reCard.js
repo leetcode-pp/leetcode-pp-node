2c55ddd9e77982e997f11f2c751bf32c65f59fff
robot <xifa@tsign.cn>
2024-01-16 22:48:21 +0800

    补卡

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "duxiyang430105".trim().toLocaleLowerCase();
+const name = "RealDuxy".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
