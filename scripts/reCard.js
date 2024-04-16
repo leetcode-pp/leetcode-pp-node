41200d9a12505c29f5f84c9be316afeb2c40bfd6
robot <xifa@tsign.cn>
2024-04-16 23:06:08 +0800

    chore: recard

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "sanjiu81".trim().toLocaleLowerCase();
+const name = "yashuning".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
