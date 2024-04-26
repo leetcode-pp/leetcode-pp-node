fdb16cc8515b885eaeae206644ee9d28390a1ef9
robot <xifa@tsign.cn>
2024-04-26 10:44:19 +0800

    feat: 2024-04

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "yashuning".trim().toLocaleLowerCase();
+const name = "rao-qianlin".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
