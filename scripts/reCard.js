e2bbe21848d15c144c3fd98c9e7b833f2b7e05cd
robot <xifa@tsign.cn>
2024-05-06 21:08:19 +0800

    feat: 2024-04

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "hermione666".trim().toLocaleLowerCase();
+const name = "yashuning".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
