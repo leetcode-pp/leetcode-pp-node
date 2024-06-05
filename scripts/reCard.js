6ca4cfd1fdf1876b2b2c8d1fea27cbd780b477ed
robot <xifa@tsign.cn>
2024-06-05 20:29:48 +0800

    feat: 2024-05

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "pandapls".trim().toLocaleLowerCase();
+const name = "luzhaofeng".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
