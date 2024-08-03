778cf2122b08b7347545f4b459b207a0b5aeac03
robot <xifa@tsign.cn>
2024-08-03 23:19:11 +0800

    prepare next

--- config/index.js
+++ config/index.js
@@ -1,6 +1,6 @@
-const startTime = new Date("2024-04-08T00:00:00.000+08:00");
+const startTime = new Date("2024-08-15T00:00:00.000+08:00");
 // const { users } = require("./users-5");
-const { users } = require("./users-13");
+const { users } = require("./users-14");
 const us = require("../static/users/index");
 
 const whitelist = [
@@ -47,7 +47,7 @@ const leetcodeConfig = {
   lcSeesionCookieName: "LEETCODE_SESSION", // lc存seesionid的 cookie键名
   lcCsrftokenCookieName: "csrftoken", // lc存csrf的 cookie键名
 };
-const season = 13;
+const season = 14;
 module.exports = {
   leetcodeConfig,
   season,
