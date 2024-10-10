211bbfef5f0c9df919a7f3d7903c7f1f0b142245
robot <xifa@tsign.cn>
2024-10-11 00:40:57 +0800

    2024-08

--- scripts/reCard.js
+++ scripts/reCard.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const name = "jialigogogo".trim().toLocaleLowerCase();
+const name = "Fightforcoding".trim().toLocaleLowerCase();
 
 const us = require("../static/users/index");
 // const delta = 3 * 24 * 60 * 60 * 1000;
